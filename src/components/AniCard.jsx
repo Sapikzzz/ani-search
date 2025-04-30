ae4c1ce8b71ad3e86fed4d351c7afa46c4085130
Sapik <2002jakubs@gmail.com>
2025-04-30 13:05:30 +0200

    Updated query and card props

--- src/components/AniCard.jsx
+++ src/components/AniCard.jsx
@@ -1,13 +1,19 @@
-export default function AniCard({ title, coverImage }) {
-	return (
-		// TODO: Title on two lines if needed, more = crop the title
-		<div className='w-64 flex flex-col items-center text-center'>
-			<img
-				src={coverImage}
-				alt='Cover Image'
-				className='rounded-2xl border w-48'
-			/>
-			<p className=' text-l font-semibold text-amber-50'>{title}</p>
-		</div>
-	);
+export default function AniCard({
+  title,
+  coverImage,
+  startDate,
+  endDate,
+  score,
+}) {
+  return (
+    // TODO: Title on two lines if needed, more = crop the title
+    <div className="w-64 flex flex-col items-center text-center">
+      <img
+        src={coverImage}
+        alt="Cover Image"
+        className="rounded-2xl border w-48"
+      />
+      <p className=" text-l font-semibold text-amber-50">{title}</p>
+    </div>
+  );
 }
