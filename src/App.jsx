a26321e01131f96f4080790beee6815c808cedb8
Sapik <2002jakubs@gmail.com>
2025-05-05 12:17:26 +0200

    Added styling for AniCard component - hover animation, title cropping

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,105 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import SearchBar from "./components/SearchBar";
-import { gql, useQuery } from "@apollo/client";
-import AniCard from "./components/AniCard";
-import PageButtons from "./components/PageButtons";
+import { useState } from 'react';
+import NavBar from './components/NavBar';
+import SearchBar from './components/SearchBar';
+import { gql, useQuery } from '@apollo/client';
+import AniCard from './components/AniCard';
+import PageButtons from './components/PageButtons';
 
 const ANIME_QUERY = gql`
-  query Media($perPage: Int, $sort: [MediaSort], $type: MediaType, $page: Int) {
-    Page(perPage: $perPage, page: $page) {
-      pageInfo {
-        currentPage
-        hasNextPage
-        perPage
-        total
-      }
-      media(sort: $sort, type: $type) {
-        title {
-          english
-          romaji
-        }
-        format
-        id
-        coverImage {
-          extraLarge
-        }
-        averageScore
-        endDate {
-          year
-          month
-        }
-        startDate {
-          month
-          year
-        }
-      }
-    }
-  }
+	query Media(
+		$perPage: Int
+		$sort: [MediaSort]
+		$type: MediaType
+		$page: Int
+	) {
+		Page(perPage: $perPage, page: $page) {
+			pageInfo {
+				currentPage
+				hasNextPage
+				perPage
+				total
+			}
+			media(sort: $sort, type: $type) {
+				title {
+					english
+					romaji
+				}
+				format
+				id
+				coverImage {
+					extraLarge
+				}
+				averageScore
+				endDate {
+					year
+					month
+					day
+				}
+				startDate {
+					month
+					year
+					day
+				}
+			}
+		}
+	}
 `;
 
 function App() {
-  const [page, setPage] = useState(1);
-  const { data, loading, error } = useQuery(ANIME_QUERY, {
-    variables: {
-      page: page,
-      perPage: 50,
-      sort: "SCORE_DESC",
-      type: "ANIME",
-    },
-  });
+	const [page, setPage] = useState(1);
+	const { data, loading, error } = useQuery(ANIME_QUERY, {
+		variables: {
+			page: page,
+			perPage: 50,
+			sort: 'SCORE_DESC',
+			type: 'ANIME',
+		},
+	});
 
-  const goToNextPage = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
+	const goToNextPage = () => {
+		setPage((prevPage) => prevPage + 1);
+	};
 
-  const goToPrevPage = () => {
-    if (page > 1) {
-      setPage((prevPage) => prevPage - 1);
-    }
-  };
+	const goToPrevPage = () => {
+		if (page > 1) {
+			setPage((prevPage) => prevPage - 1);
+		}
+	};
 
-  if (loading) return "Loading...";
-  if (error) return <pre>{error.message}</pre>;
+	if (loading) return 'Loading...';
+	if (error) return <pre>{error.message}</pre>;
 
-  return (
-    <>
-      <NavBar></NavBar>
-      <div className="bg-blue-900 p-8 flex flex-col gap-8">
-        <SearchBar></SearchBar>
-        <div className="grid grid-cols-5 gap-16">
-          {data.Page.media.map((mediaItem) => (
-            <div key={mediaItem.id} className="flex flex-row justify-center">
-              <AniCard
-                title={mediaItem.title.english}
-                coverImage={mediaItem.coverImage.extraLarge}
-                startDate={mediaItem.startDate}
-                endDate={mediaItem.endDate}
-                score={mediaItem.averageScore}
-              >
-                {/* {console.log(mediaItem.title.english)} */}
-              </AniCard>
-            </div>
-          ))}
-        </div>
-        <PageButtons
-          goToNextPage={goToNextPage}
-          goToPrevPage={goToPrevPage}
-          page={page}
-        ></PageButtons>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<NavBar></NavBar>
+			<div className='bg-blue-900 p-8 flex flex-col gap-8'>
+				<SearchBar></SearchBar>
+				<div className='grid grid-cols-5 gap-16'>
+					{data.Page.media.map((mediaItem) => (
+						<div
+							key={mediaItem.id}
+							className='flex flex-row justify-center'
+						>
+							<AniCard
+								title={mediaItem.title.english}
+								coverImage={mediaItem.coverImage.extraLarge}
+								startDate={mediaItem.startDate}
+								endDate={mediaItem.endDate}
+								score={mediaItem.averageScore}
+							>
+								{console.log(mediaItem.startDate)}
+							</AniCard>
+						</div>
+					))}
+				</div>
+				<PageButtons
+					goToNextPage={goToNextPage}
+					goToPrevPage={goToPrevPage}
+					page={page}
+				></PageButtons>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/AniCard.jsx
+++ src/components/AniCard.jsx
@@ -1,19 +1,33 @@
 export default function AniCard({
-  title,
-  coverImage,
-  startDate,
-  endDate,
-  score,
+	title,
+	coverImage,
+	startDate,
+	endDate,
+	score,
 }) {
-  return (
-    // TODO: Title on two lines if needed, more = crop the title
-    <div className="w-64 flex flex-col items-center text-center">
-      <img
-        src={coverImage}
-        alt="Cover Image"
-        className="rounded-2xl border w-48"
-      />
-      <p className=" text-l font-semibold text-amber-50">{title}</p>
-    </div>
-  );
+	return (
+		<div className='w-64 flex flex-col items-center text-center'>
+			<div className='relative group'>
+				<div className='bg-black absolute w-full h-full rounded-2xl opacity-0 group-hover:opacity-70 transition-opacity duration-100'></div>
+				<div className='absolute top-4 left-4 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+					Score: {score}
+				</div>
+				<div className='absolute top-12 left-4 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+					Start Date: {startDate.day}.{startDate.month}.
+					{startDate.year}
+				</div>
+				<div className='absolute top-20 left-4 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+					End Date: {endDate.day}.{endDate.month}.{endDate.year}
+				</div>
+				<img
+					src={coverImage}
+					alt='Cover Image'
+					className='rounded-2xl border w-48'
+				/>
+			</div>
+			<p className=' text-l font-semibold text-amber-50 line-clamp-2 overflow-hidden text-ellipsis'>
+				{title}
+			</p>
+		</div>
+	);
 }
